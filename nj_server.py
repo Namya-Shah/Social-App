73efb17e89eb059c6148aff5185da2f5346c1d09
Namya Shah <85009103+Namya-Shah@users.noreply.github.com>
2025-12-09 19:27:10 +0530

    codes

---
+++ nj_server.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+catalog = {
+    "tomatoes": {
+        "units": "boxes",
+        "qty": 1000
+    },
+    "wine": {
+        "units": "bottles",
+        "qty": 500
+    }
+}
+
+app = FastAPI(title="New Jersey API Server")
+
+@app.get("/warehouse/{product}")
+async def load_truck(product,order_qty):
+    return {
+        "product": product,
+        "order_qty": order_qty,
+        "units": catalog[product]["units"]
+    }
